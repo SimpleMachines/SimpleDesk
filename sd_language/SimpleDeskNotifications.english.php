d33e60ac6ff9ef2f61e1b8f2074d3297ac010005
gruffen <gruffen@simpledesk.net>
2011-05-30 23:12:03 +0000

    ! Tweak templates to add the ticket id and poster's name into the body of the email as well as the subject where appropriate. Format used is like: 'A new helpdesk ticket, "[{ticket_id}] {subject}", has been created, by {poster_name}.' - this way, the ticket id is used in a format consistent with everywhere else in the helpdesk, and it includes the poster's name in a readable format. [Feature 707]

--- sd_language/SimpleDeskNotifications.english.php
+++ sd_language/SimpleDeskNotifications.english.php
@@ -31,36 +31,36 @@
 
 // Email subject and body contents
 $txt['template_subject_notify_new_ticket'] = 'Helpdesk [{ticket_id}] New ticket from [{poster_name}] -RE- [{subject}]';
-$txt['template_body_notify_new_ticket'] = 'A new helpdesk ticket, "{subject}", has been created.' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
-$txt['template_bodyfull_notify_new_ticket'] = 'A new helpdesk ticket, "{subject}", has been created.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_new_ticket'] = 'A new helpdesk ticket, "[{ticket_id}] {subject}", has been created, by {poster_name}.' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
+$txt['template_bodyfull_notify_new_ticket'] = 'A new helpdesk ticket, "[{ticket_id}] {subject}", has been created, by {poster_name}.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_new_ticket'] = 'New ticket';
 
 $txt['template_subject_notify_new_reply_own'] = 'Helpdesk [{ticket_id}] New reply from [{poster_name}] -RE- [{subject}]';
-$txt['template_body_notify_new_reply_own'] = 'Your ticket, "{subject}", has been replied to.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
-$txt['template_bodyfull_notify_new_reply_own'] = 'Your ticket, "{subject}", has been replied to.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_new_reply_own'] = 'Your ticket, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_bodyfull_notify_new_reply_own'] = 'Your ticket, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_new_reply_own'] = 'User updated';
 
 $txt['template_subject_notify_new_reply_assigned'] = 'Helpdesk [{ticket_id}] New reply from [{poster_name}] -RE- [{subject}]';
-$txt['template_body_notify_new_reply_assigned'] = 'A ticket assigned to you, "{subject}", has been replied to.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
-$txt['template_bodyfull_notify_new_reply_assigned'] = 'A ticket assigned to you, "{subject}", has been replied to.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_new_reply_assigned'] = 'A ticket assigned to you, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_bodyfull_notify_new_reply_assigned'] = 'A ticket assigned to you, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_new_reply_assigned'] = 'Assigned ticket updated';
 
 $txt['template_subject_notify_new_reply_previous'] = 'Helpdesk [{ticket_id}] New reply from [{poster_name}] -RE- [{subject}]';
-$txt['template_body_notify_new_reply_previous'] = 'A ticket you previously replied to, "{subject}", has been replied to again.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
-$txt['template_bodyfull_notify_new_reply_previous'] = 'A ticket you previously replied to, "{subject}", has been replied to again.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_new_reply_previous'] = 'A ticket you previously replied to, "[{ticket_id}] {subject}", has been replied to again, by {poster_name}.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_bodyfull_notify_new_reply_previous'] = 'A ticket you previously replied to, "[{ticket_id}] {subject}", has been replied to again, by {poster_name}.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_new_reply_previous'] = 'Another reply';
 
 $txt['template_subject_notify_new_reply_any'] = 'Helpdesk [{ticket_id}] New reply from [{poster_name}] -RE- [{subject}]';
-$txt['template_body_notify_new_reply_any'] = 'A helpdesk ticket, "{subject}", has been replied to.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
-$txt['template_bodyfull_notify_new_reply_any'] = 'A helpdesk ticket, "{subject}", has been replied to.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_new_reply_any'] = 'A helpdesk ticket, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
+$txt['template_bodyfull_notify_new_reply_any'] = 'A helpdesk ticket, "[{ticket_id}] {subject}", has been replied to, by {poster_name}.' . "\n\n" . '====Begin Comment====' . "\n" . '{body}' . "\n" . '====End Comment====' . "\n\n" . 'You can read the reply here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_new_reply_any'] = 'New reply';
 
 $txt['template_subject_notify_assign_me'] = 'Helpdesk [{ticket_id}] Ticket assigned: [{subject}]';
-$txt['template_body_notify_assign_me'] = 'The helpdesk ticket, "{subject}", has been assigned to you.' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_assign_me'] = 'The helpdesk ticket, "[{ticket_id}] {subject}", has been assigned to you.' . "\n\n" . 'You can access it here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_assign_me'] = 'Assigned to';
 
 $txt['template_subject_notify_assign_own'] = 'Helpdesk [{ticket_id}] Ticket assigned: [{subject}]';
-$txt['template_body_notify_assign_own'] = 'Your helpdesk ticket, "{subject}", has been assigned to a staff member.' . "\n\n" . 'You can access your ticket here:' . "\n" . '{ticketlink}';
+$txt['template_body_notify_assign_own'] = 'Your helpdesk ticket, "[{ticket_id}] {subject}", has been assigned to a staff member.' . "\n\n" . 'You can access your ticket here:' . "\n" . '{ticketlink}';
 $txt['template_log_notify_assign_own'] = 'Assigned to staff';
 
 ?>
