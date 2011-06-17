e55440cff656fba99178617510e5509925aea0ee
gruffen <gruffen@simpledesk.net>
2011-06-17 14:46:42 +0000

    ! Silent update only applies to new replies, not edited ones [Bug 765]

--- sd_source/SimpleDesk-Post.php
+++ sd_source/SimpleDesk-Post.php
@@ -794,7 +794,7 @@ function shd_post_reply()
 		'disable_smileys' => !$new_reply ? !empty($_REQUEST['no_smileys']) : ($ticketinfo['smileys_enabled'] == 0),
 	);
 	$context['can_solve'] = (shd_allowed_to('shd_resolve_ticket_any', $ticketinfo['dept']) || (shd_allowed_to('shd_resolve_ticket_own', $ticketinfo['dept']) && $ticketinfo['starter_id'] == $user_info['id']));
-	$context['can_silent_update'] = shd_allowed_to('shd_silent_update', $ticketinfo['dept']);	
+	$context['can_silent_update'] = $new_reply && shd_allowed_to('shd_silent_update', $ticketinfo['dept']);	
 	shd_posting_additional_options();
 	shd_load_canned_replies();
 
@@ -1023,7 +1023,7 @@ function shd_save_reply()
 		'disable_smileys' => !empty($_REQUEST['no_smileys']),
 	);
 	$context['can_solve'] = (shd_allowed_to('shd_resolve_ticket_any', $ticketinfo['dept']) || (shd_allowed_to('shd_resolve_ticket_own', $ticketinfo['dept']) && $ticketinfo['starter_id'] == $user_info['id']));
-	$context['can_silent_update'] = shd_allowed_to('shd_silent_update', $ticketinfo['dept']);
+	$context['can_silent_update'] = $new_reply && shd_allowed_to('shd_silent_update', $ticketinfo['dept']);
 	$context['log_action'] = $new_reply ? 'newreply' : 'editreply';
 	$context['log_params']['subject'] = $context['ticket_form']['subject'];
 	shd_posting_additional_options();
