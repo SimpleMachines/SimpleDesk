2560d08a3c391689a1665993c0b0ee26719685c0
jdarwood007 <unmonitored+github@sleepycode.com>
2019-02-10 11:10:56 -0800

    Fix the copyright year

--- sd_source/SimpleDesk-AdminCannedReplies.php
+++ sd_source/SimpleDesk-AdminCannedReplies.php
@@ -5,7 +5,7 @@
 #       An advanced help desk modification built on SMF       #
 ###############################################################
 #                                                             #
-#         * Copyright 2018 - SimpleDesk.net                   #
+#         * Copyright 2019 - SimpleDesk.net                   #
 #                                                             #
 #   This file and its contents are subject to the license     #
 #   included with this distribution, license.txt, which       #
