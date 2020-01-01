9d1823c6171694848294940c2521c61466c6ad5d
jdarwood007 <unmonitored+github@sleepycode.com>
2020-01-01 09:37:07 -0800

    Update to 2020 year

--- sd_source/Subs-SimpleDesk.php
+++ sd_source/Subs-SimpleDesk.php
@@ -5,7 +5,7 @@
 *       An advanced help desk modification built on SMF       *
 ***************************************************************
 *                                                             *
-*         * Copyright 2019 - SimpleDesk.net                   *
+*         * Copyright 2020 - SimpleDesk.net                   *
 *                                                             *
 *   This file and its contents are subject to the license     *
 *   included with this distribution, license.txt, which       *
