2c1b30ece01388b79aa002cf240060a242a79398
Jeremy D <jdarwood007@users.noreply.github.com>
2023-05-07 14:15:44 -0700

    New translations SDPluginFrontPage.english.php (greek)

---
+++ el/sd_plugins_lang/SDPluginFrontPage.greek.php
@@ -0,0 +1,38 @@
+<?php
+/**************************************************************
+*          Simple Desk Project - www.simpledesk.net           *
+***************************************************************
+*       An advanced help desk modification built on SMF       *
+***************************************************************
+*                                                             *
+*         * Copyright 2023 - SimpleDesk.net                   *
+*                                                             *
+*   This file and its contents are subject to the license     *
+*   included with this distribution, license.txt, which       *
+*   states that this software is New BSD Licensed.            *
+*   Any questions, please contact SimpleDesk.net              *
+*                                                             *
+***************************************************************
+* SimpleDesk Version: 2.1.0                                   *
+* File Info: SDPluginFrontPage.english.php                    *
+**************************************************************/
+// Version: 2.1; SimpleDesk main language file
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+$txt['shdp_frontpage'] = 'Front Page';
+$txt['shdp_frontpage_desc'] = 'This plugin allows you to create a front page in place of the ticket listing, for news, announcements and so on.';
+$txt['shdp_frontpage_main_desc'] = 'This area allows you to configure the front page of the helpdesk, when it is presented and what it contains.';
+
+$txt['shdp_frontpage_type'] = 'Type of front page content';
+$txt['shdp_frontpage_type_php'] = 'Raw PHP';
+$txt['shdp_frontpage_type_bbcode'] = 'Bulletin board code (as given in the main forum configuration)';
+
+$txt['shdp_frontpage_appear'] = 'When it should appear';
+$txt['shdp_frontpage_appear_always'] = 'Always as the front page';
+$txt['shdp_frontpage_appear_firstload'] = 'Only at the first time of login (and then remain hidden)';
+$txt['shdp_frontpage_appear_firstdefault'] = 'At the first time of login (with tickets being the default "home")';
+
+$txt['shdp_frontpage_content'] = 'Main content';
+
+$txt['shdp_tickets'] = 'Εισιτήρια';
