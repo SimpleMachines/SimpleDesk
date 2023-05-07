76dbb78b678a6ff61edaa8ec68ed5b8510a70ed9
Jeremy D <jdarwood007@users.noreply.github.com>
2023-05-07 13:40:52 -0700

    New translations SimpleDeskWho.english.php (Hebrew)

---
+++ he/sd_language/SimpleDeskWho.Hebrew.php
@@ -0,0 +1,80 @@
+<?php
+/**************************************************************
+*          Simple Desk Project - www.simpledesk.net           *
+***************************************************************
+*       An advanced help desk modification built on SMF       *
+***************************************************************
+*                                                             *
+*         * Copyright 2023 - SimpleDesk.net                   *
+*                                                             *
+*   This file and its contents are subject to the license     *
+*   included with this distribution, license.txt, which       *
+*   states that this software is New BSD Licensed.            *
+*   Any questions, please contact SimpleDesk.net              *
+*                                                             *
+***************************************************************
+* SimpleDesk Version: 2.1.0                                   *
+* File Info: SimpleDeskWho.english.php                        *
+**************************************************************/
+// Version: 2.1; SimpleDesk who's online
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+/**
+ *	This file contains all of the language strings used by Who's Online for actions carried out within the helpdesk.
+ *
+ *	@package language
+ *	@todo Document the text groups in this file.
+ *	@since 1.0
+ */
+
+//! @name General strings
+//@{
+$txt['whoall_helpdesk'] = 'Viewing the helpdesk index.';
+$txt['whoall_helpdesk_main'] = 'Viewing the helpdesk index.';
+$txt['whoall_helpdesk_ticket'] = 'Viewing a helpdesk ticket.';
+//@}
+
+//! @name Posting/editing
+//@{
+$txt['whoall_helpdesk_newticket'] = 'Posting a new ticket in the helpdesk.';
+$txt['whoall_helpdesk_editticket'] = 'Editing a ticket in the helpdesk.';
+$txt['whoall_helpdesk_saveticket'] = 'Posting a ticket in the helpdesk.';
+$txt['whoall_helpdesk_reply'] = 'Replying to a helpdesk ticket.';
+$txt['whoall_helpdesk_savereply'] = 'Replying to a helpdesk ticket.';
+$txt['whoall_helpdesk_editreply'] = 'Editing a reply on a helpdesk ticket.';
+$txt['whoall_helpdesk_markunread'] = 'Marking a helpdesk ticket as unread.';
+//@}
+
+//! @name Updating ticket details
+//@{
+$txt['whoall_helpdesk_assign'] = 'Assigning a ticket to a user in the helpdesk.';
+$txt['whoall_helpdesk_assign2'] = 'Assigning a ticket to a user in the helpdesk.';
+$txt['whoall_helpdesk_ajax'] = 'Updating a ticket in the helpdesk.';
+$txt['whoall_helpdesk_privacychange'] = 'Updating a ticket\'s details in the helpdesk.';
+$txt['whoall_helpdesk_urgencychange'] = 'Updating a ticket\'s details in the helpdesk.';
+//@}
+
+//! @name Deletion/recycling, restoration, permadeletion
+//@{
+$txt['whoall_helpdesk_deletereply'] = 'Deleting a reply to a helpdesk ticket.';
+$txt['whoall_helpdesk_deleteticket'] = 'Deleting a ticket in the helpdesk.';
+$txt['whoall_helpdesk_restorereply'] = 'Restoring a post from the helpdesk recycle bin.';
+$txt['whoall_helpdesk_restoretopic'] = 'Restoring a ticket from the helpdesk recycle bin.';
+$txt['whoall_helpdesk_recyclebin'] = 'Viewing the deleted tickets in the helpdesk.';
+$txt['whoall_helpdesk_permadelete'] = 'Permanently deleting a ticket from the helpdesk.';
+//@}
+
+//! @name Ticket resolution
+//@{
+$txt['whoall_helpdesk_resolveticket'] = 'Marking a ticket as resolved, or reopening a resolved, ticket in the helpdesk.';
+$txt['whoall_helpdesk_closedtickets'] = 'Viewing the resolved tickets in the helpdesk.';
+//@}
+
+//! @name Topic-to-ticket, ticket-to-topic movement
+//@{
+$txt['whoall_helpdesk_tickettotopic'] = 'Moving a helpdesk ticket to a forum topic.';
+$txt['whoall_helpdesk_tickettotopic2'] = 'Moving a helpdesk ticket to a forum topic.';
+$txt['whoall_helpdesk_topictoticket'] = 'Moving a forum topic to a helpdesk ticket.';
+$txt['whoall_helpdesk_topictoticket2'] = 'Moving a forum topic to a helpdesk ticket.';
+//@}
