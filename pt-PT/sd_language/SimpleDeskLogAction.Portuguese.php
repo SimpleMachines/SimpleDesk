c8a1bc6c526ca6d9160e0b90058471f20c7eda80
Jeremy D <jdarwood007@users.noreply.github.com>
2023-05-07 14:13:44 -0700

    New translations SimpleDeskLogAction.english.php (Portuguese)

--- pt-PT/sd_language/SimpleDeskLogAction.Portuguese.php
+++ pt-PT/sd_language/SimpleDeskLogAction.Portuguese.php
@@ -132,7 +132,7 @@ $txt['shd_log_notify_hiddenemail'] = '%1$d other email addresses';
 $txt['shd_log_notify_hiddenemail_1'] = '1 other email address';
 $txt['shd_log_notify_users'] = 'users';
 $txt['shd_log_notify_email'] = 'email';
-$txt['shd_log_notifications'] = 'Notifications';
+$txt['shd_log_notifications'] = 'notificações';
 $txt['shd_log_unknown_user_1'] = '1 former user';
 $txt['shd_log_unknown_user_n'] = '%1$d former users';
 $txt['shd_log_monitor'] = 'Added &quot;<a href="{scripturl}?action=helpdesk;sa=ticket;ticket={ticket}">{subject}</a>&quot; to their monitor list.';
