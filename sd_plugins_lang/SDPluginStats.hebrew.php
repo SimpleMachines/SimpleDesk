9e46bc2707857d31e580f2e53f50ab1e4497f225
Jeremy D <jdarwood007@users.noreply.github.com>
2023-05-07 14:31:38 -0700

    New translations SDPluginStats.english.php (hebrew)

---
+++ sd_plugins_lang/SDPluginStats.hebrew.php
@@ -0,0 +1,30 @@
+<?php
+/**************************************************************
+*          Simple Desk Project - www.simpledesk.net           *
+***************************************************************
+*       An advanced help desk modification built on SMF       *
+***************************************************************
+*                                                             *
+*         * Copyright 2023 - SimpleDesk.net                   *
+*                                                             *
+*   This file and its contents are subject to the license     *
+*   included with this distribution, license.txt, which       *
+*   states that this software is New BSD Licensed.            *
+*   Any questions, please contact SimpleDesk.net              *
+*                                                             *
+***************************************************************
+* SimpleDesk Version: 2.1.0                                   *
+* File Info: SDPluginStats.english.php                        *
+**************************************************************/
+// Version: 2.1; SimpleDesk main language file
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+$txt['shdp_stats'] = 'Statistics';
+$txt['shdp_stats_desc'] = 'This plugin allows you to view statistics of your helpdesk.';
+
+$txt['shdp_enable_stats'] = 'Enable stats';
+
+
+$txt['shdp_stats_main'] = 'j';
+$txt['shdp_stats_main_desc'] = 'Stats';
