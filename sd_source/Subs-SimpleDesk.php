41afc87c1e3ce49b99ddf9639f59e11970e1a3e1
jdarwood007 <unmonitored+github@sleepycode.com>
2025-07-25 05:49:25 -0700

    Minor tweaks for versioning

--- sd_source/Subs-SimpleDesk.php
+++ sd_source/Subs-SimpleDesk.php
@@ -13,7 +13,7 @@
 *   Any questions, please contact SimpleDesk.net              *
 *                                                             *
 ***************************************************************
-* SimpleDesk Version: 2.1.3                                   *
+* SimpleDesk Version: 2.1.5                                   *
 * File Info: Subs-SimpleDesk.php                              *
 **************************************************************/
 
